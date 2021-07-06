6d3740c98eb6cb562b2976d5e35cea3f380137d4
vagrant <vhuang5564@gmail.com>
2021-07-06 06:52:01 +0000

    added gitignore, views folder to show html style script, and updated express_server.js to respond with ejs template

---
+++ express_server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+
+app.set("view engine", "ejs");
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
+ 
+ app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });
+
+ app.get('/urls', (req, res) => {
+   const templateVars = { urls: urlDatabase };
+   res.render('urls_index', templateVars);
+ });
+
+ app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL/* What goes here? */ };
+  res.render("urls_show", templateVars);
+});
