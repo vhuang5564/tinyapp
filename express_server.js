cf0792ca935da4a466fd63e7593fef4cdd3a9983
vagrant <vhuang5564@gmail.com>
2021-07-08 05:48:15 +0000

    included header partial in all ejs files

--- express_server.js
+++ express_server.js
@@ -18,6 +18,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
 function generateRandomString() {
   return Math.random().toString(36).substring(2, 8)
 }
@@ -96,10 +109,27 @@ app.get('/login', (req,res) => { // renders login
 
 app.post('/login', (req, res) => { // /login
   res.cookie('username', req.body['username'])
-  res.redirect('/login');
+  res.redirect('/urls');
 })
 
 app.post('/logout', (req, res) => { // logout
   res.clearCookie('username');
   res.redirect('/urls');
+})
+
+app.get('/register', (req, res) => { // renders register page
+  res.render('register')
+})
+
+app.post('/register', (req, res) => { // register
+  const email = req.body['email'];
+  const password = req.body['password'];
+  const id = generateRandomString()
+  const newUser = id
+  users[newUser]['id'] = newUser;
+  users[newUser]['email'] = email;
+  users[newUser]['password'] = password;
+  console.log(users);
+
+  res.redirect('/register');
 })
